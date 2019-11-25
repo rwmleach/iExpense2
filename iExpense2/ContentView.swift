8c00365a06344d7b889d682ac5a4e668535a1495
Ryan Leach <rwmleach@gmail.com>
2019-11-25 10:55:23 -0500

    Initial Commit

---
+++ iExpense2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense2
+//
+//  Created by Ryan Leach on 11/25/19.
+//  Copyright © 2019 Ryan Leach. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
